c9a9ab09a4173222d5a7c74555685463e100f243
Shivam Pathak <sp3777472@gmail.com>
2026-08-07 09:44:05 +0530

    Fix TemplateResponse

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html"
     )
 
 
