3afe801aa9f844899a580c307cd693a246a6b986
Shivam Pathak <sp3777472@gmail.com>
2026-08-07 10:52:51 +0530

    Fix upload and template

--- app.py
+++ app.py
@@ -27,10 +27,9 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        request=request,
-        name="index.html"
-    )
-
+    "index.html",
+    {"request": request}
+)
 
 @app.post("/upload")
 async def upload_resume(file: UploadFile = File(...)):
